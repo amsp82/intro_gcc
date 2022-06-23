4bf91b8437d5f143fac721e46e0f068199532c27
Adelmo Menezes - narnia <adelmo.msp@gmail.com>
2022-06-23 07:46:15 -0300

    Como fazer comentários em meu código C

--- aula_002/main.c
+++ aula_002/main.c
@@ -3,7 +3,6 @@
 /*
     Aula: 002
     Tema: Como fazer comentários em meu código C
-    Link: https://www.youtube.com/watch?v=yrWIlIVZHB0&list=PLqJK4Oyr5WSjjEQCKkX6oXFORZX7ro3DA&index=2
     Data: 22.06.2022
 */
 
@@ -15,7 +14,7 @@ int main()
         bloco de
         comentário
     */
-    printf("Aprendendo comentários na linguagem C\n");
+    printf("Aprendendo comentários na linguagem C.\n");
     // printf("Hello world!\n");
     return 0;
 }
